9a37a9162410c12d053255f3c235c192b0c269d2
Josh <critesjosh@gmail.com>
2017-08-30 22:10:55 -0400

    hide passwords and let the initiator reclaim funds
    make passwords hidden in a hash. add reclaim funds function.  restructure data.

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -3,21 +3,32 @@ pragma solidity ^0.4.6;
 contract Remittance {
 
 	address public owner;
-	uint    public deadline;
 	uint    public fee = 10;
+	uint    public id;
+	uint    public ownerBalance;
 
-	mapping (address => uint)    public balances;
-	mapping (address => bytes32) public unlockCodes;
+	mapping (uint => Exchange) public exchanges;
+
+	struct Exchange {
+		bytes32 unlockCode;
+		uint    balance;
+		address exchanger;
+		uint    deadline;
+		address initiator;
+	}
 
 	event LogInit(address sender, address exchangeShop, uint duration, uint amount);
 	event LogWithdrawal(address recipient, uint amount);
+	event LogReclaim(address recipient, uint amount);
+
 
 	function Remittance()
 	{
 		owner = msg.sender;
 	}
 
-	function init(uint duration, address exchangeShop, bytes32 pw1, bytes32 pw2)
+	//init with a hash of the two passwords and the address of the exchanger
+	function init(uint duration, address exchangeShop, bytes32 hash)
 		public
 		payable
 		returns(bool success)
@@ -26,32 +37,44 @@ contract Remittance {
 		require(duration < 1000);
 
 		uint amountToExchange = msg.value - fee;
-		deadline = block.number + duration;
+		uint deadline = block.number + duration;
 
-		bytes32 unlockCode = keccak256(pw1, pw2, exchangeShop);
-		unlockCodes[exchangeShop] = unlockCode;
+		Exchange memory exchange = Exchange(hash, amountToExchange, exchangeShop, deadline, msg.sender);
+		exchanges[id] = exchange;
 
-		balances[exchangeShop] += amountToExchange;
-		balances[owner] += fee;
-
+		id++;
+		ownerBalance += fee;
 		LogInit(msg.sender, exchangeShop, duration, amountToExchange);
 		return true;
 	}
 
-	//hash the password client side on the webpage
-	//we only need to use 1 password that was given to Bob + Carol's address
-	function checkPasswordHashAndTranferFunds(bytes32 pw1, bytes32 pw2)
+	function checkPasswordHashAndTranferFunds(uint exchangeID, bytes32 pw1, bytes32 pw2)
 		public
 		returns(bool success)
 	{	
-		require(balances[msg.sender] > 0);
-		bytes32 unlockCode = keccak256(pw1, pw2, msg.sender);
-		require(unlockCodes[msg.sender] == unlockCode);
+		require(exchanges[exchangeID].balance > 0);
+		bytes32 unlockAttempt = keccak256(pw1, pw2, msg.sender);
+		require(exchanges[exchangeID].unlockCode == unlockAttempt);
 
-		uint amount = balances[msg.sender];
-		balances[msg.sender] = 0;
+		uint amount = exchanges[id].balance;
+		exchanges[id].balance = 0;
 		msg.sender.transfer(amount);
 		LogWithdrawal(msg.sender, amount);
+		return true;
+	}
+
+	function reclaimFunds(uint exchangeID)
+		public
+		returns(bool success)
+	{
+		require(exchanges[exchangeID].initiator == msg.sender);
+		require(exchanges[exchangeID].deadline <= block.number);
+		require(exchanges[exchangeID].balance > 0);
+
+		uint amount = exchanges[exchangeID].balance;
+		exchanges[exchangeID].balance = 0;
+		msg.sender.transfer(amount);
+		LogReclaim(msg.sender, amount);
 		return true;
 	}
 	
@@ -60,11 +83,10 @@ contract Remittance {
 		returns(bool success)
 	{
 		require(msg.sender == owner);
-		require(balances[owner] > 0);
+		require(ownerBalance > 0);
 
-		uint amount = balances[owner];
-		balances[owner] = 0;
-
+		uint amount = ownerBalance;
+		ownerBalance = 0;
 		owner.transfer(amount);
 		LogWithdrawal(owner, amount);
 		return true;
